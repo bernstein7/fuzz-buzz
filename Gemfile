a11c2812b213eeb27a22203e5146ac5572f2951f
bernstein <clapandslap@gmail.com>
2016-12-14 14:49:06 +0200

    Add api endpoint for checking result

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-
+gem 'sinatra-json'
 gem 'thin'
 
 group :development do
--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
+require 'sinatra/json'
+require_relative 'services/answers_service.rb'
 
 module FizzBuzz
   class Application < Sinatra::Base
+
+    post '/get_result' do
+      service = AnswersService.new
+      json result: service.check(number)
+    end
+
+    def number
+      params[:number].to_i
+    end
   end
 end
